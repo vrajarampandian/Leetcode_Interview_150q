774e8b61ded27b8b97460a0a01999ab64f748a2a
rajaram <vrajarampandian@gmail.com>
2025-04-20 11:06:46 +0530

    updated the code

--- remove_element.cpp
+++ remove_element.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class solution
 {
     public:
-    void removeelement(vector<int> &nums, int v)
+    int removeelement(vector<int> &nums, int v)
     {
         // Your commented approach:
         // The following approach is problematic because modifying a container
@@ -25,7 +25,18 @@ class solution
         // 1. std::remove shifts all elements not equal to 'v' to the front of the vector
         //    and returns an iterator pointing to the new logical end of the vector.
         // 2. nums.erase removes the "garbage" values left at the end of the vector.
-        nums.erase(remove(nums.begin(), nums.end(), v), nums.end());
+        //nums.erase(remove(nums.begin(), nums.end(), v), nums.end());
+
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != v)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
 };
 
@@ -38,12 +49,19 @@ int main()
     solution s1;
 
     // Call the removeelement function to remove all occurrences of the value 4
-    s1.removeelement(nums, 4);
+    cout << "Nums size " << nums.size() << "\n"; 
+    int nsize = s1.removeelement(nums, 4);
 
     // Print the modified vector after removing the value 4
     for (auto it = nums.begin(); it != nums.end(); it++)
     {
         cout << *it << " "; // Output each element of the vector
+    }
+    cout << "\n";
+    cout << "New size " << nsize << "\n";
+    for(int i =0; i< nsize; i++)
+    {
+        cout << nums[i] << " ";
     }
 
     return 0; // Indicate successful program termination
